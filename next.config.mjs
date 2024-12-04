f5f6b0e8d0d0a2d94e3a2fce39ae5cac01955918
byteom <123omsinghpipariya@gmail.com>
2024-12-04 21:02:00 +0530

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,26 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         unoptimized: true
     },
     webpack: (config, { isServer }) => {
+        // Use asset/source instead of raw-loader for md files
         config.module.rules.push({
             test: /\.md$/,
-            use: 'raw-loader'
+            type: 'asset/source'
         });
 
         return config;
     }
 };
 
-
+// Ensure the dev platform setup is only done in development
 if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
-
+    (async () => {
+        await setupDevPlatform();
+    })();
+}
 
 export default nextConfig;
