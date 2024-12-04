178f7c1cef473fe855454d4265c8ce3fb8be32fa
byteom <123omsinghpipariya@gmail.com>
2024-12-04 19:37:42 +0530

    h

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -12,5 +13,11 @@ const nextConfig = {
         return config;
     }
 };
+
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
+
 
 export default nextConfig;
